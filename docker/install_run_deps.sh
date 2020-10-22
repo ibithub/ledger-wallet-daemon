c0a7659c0ca904ddc94c6659b18839c5422318fe
Miguel Basire <miguel.basire-ext@ledger.fr>
2020-10-22 17:00:28 +0200

    BACK-1012 : Add datadog agent on wallet-daemon exec

--- docker/install_run_deps.sh
+++ docker/install_run_deps.sh
@@ -12,6 +12,9 @@ apt-get install -yq curl netcat iputils-ping iproute2 lsof procps
 # Needed when activating PG Support on WD
 apt-get install -yq libpq-dev
 
+# Download datadog agents
+wget -O dd-java-agent.jar https://dtdg.co/latest-java-tracer
+
 # Cleanup
 apt-get clean
 rm -rf -- /var/lib/apt/lists/*
--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-OPTS="-http.port=:${HTTP_PORT} -admin.port=:${ADMIN_PORT}"
+OPTS="-http.port=:${HTTP_PORT} -admin.port=:${ADMIN_PORT} -javaagent:./dd-java-agent.jar"
 
 if [ -n "${WALLET_PROXY_ENABLED+x}" ] && [ "${WALLET_PROXY_ENABLED}" = "true" ];then
   OPTS="${OPTS} -Dhttp.proxyHost=${WALLET_PROXY_HOST} -Dhttp.proxyPort=${WALLET_PROXY_PORT}"
 fi
+
+export DD_SERVICE=wallet-daemon
+export DD_LOGS_INJECTION=true
+export DD_AGENT_HOST=172.17.0.1
+export DD_PROFILING_ENABLED=true
+export DD_TRACE_DEBUG=true
+
 exec ./bin/wallet-daemon ${OPTS} $@
