32a3bf56f0463a24038dfdd0e613128905b1c426
Thomas Wilgenbus <thomas.wilgenbus-ext@ledger.fr>
2020-12-02 13:58:49 +0100

    Add assertRepushAccountOperations

--- src/it/scala/co/ledger/wallet/daemon/utils/APIFeatureTest.scala
+++ src/it/scala/co/ledger/wallet/daemon/utils/APIFeatureTest.scala
@@ -69,6 +69,10 @@ trait APIFeatureTest extends FeatureTest with Mockito {
     server.httpPost("/pools/operations/synchronize", "", headers = defaultHeaders, andExpect = expected)
   }
 
+  def assertRepushAccountOperations(poolName: String, walletName: String, accIdx: Int, expected: Status): Response = {
+    server.httpPost(s"/pools/$poolName/wallets/$walletName/accounts/$accIdx/operations/repush", "", headers = defaultHeaders, andExpect = expected)
+  }
+
   def assertSyncAccount(poolName: String, walletName: String, accIdx: Int): Response = {
     server.httpPost(s"/pools/$poolName/wallets/$walletName/accounts/$accIdx/operations/synchronize", "", headers = defaultHeaders, andExpect = Status.Ok)
   }
