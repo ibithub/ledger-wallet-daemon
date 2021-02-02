156f8ab52a67aca49e48a1bcf0f6fee6a775a39d
RomanWlm <roman.wilhelm@ledger.fr>
2021-02-02 14:12:07 +0100

    BACK-1218 - Add datadog agent (#332)
    * add agent with stb agent plugin
    * run.sh is passing datadog agent variable only when DATADOG HOST is declared
    * Do not enb"ed datadog agent when DD agent host is declared (native
    packager agent script)
    * Log proxy and datadog whebn enabled on run.sh

--- docker/install_run_deps.sh
+++ docker/install_run_deps.sh
@@ -6,11 +6,11 @@ mkdir -p /app/database # for sqlite (can be mount outside the container at runle
 # Debug tools untils we have our ledger-stretch-slim image
 apt-get update && apt-get install -yq curl netcat iputils-ping iproute2 lsof procps
 
-# Debug tools untils we have our ledger-stretch-slim image
-apt-get install -yq curl netcat iputils-ping iproute2 lsof procps
-
 # Needed when activating PG Support on WD
 apt-get install -yq libpq-dev
+
+# Dependencies of libcore
+apt-get install -yq libxext6 libxrender1 libxtst6
 
 # Cleanup
 apt-get clean
