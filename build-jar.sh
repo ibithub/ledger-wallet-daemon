f02633fb40c33e374680025dd80bacfb5c39a77f
Thomas Wilgenbus <thomas.wilgenbus-ext@ledger.fr>
2021-02-04 14:08:58 +0100

    Bump libcore to 4.0.0-rc-44b1f7

--- build-jar.sh
+++ build-jar.sh
@@ -18,7 +18,7 @@ MACOS_BUILD_DIR=$CURRENT_DIR/../lib-ledger-core-build
 LINUX_BUILD_DIR=$CURRENT_DIR/../lib-ledger-core-build-linux
 JAR_BUILD_DIR=$CURRENT_DIR/../build-jar
 LIBCORE_AWS_URL_BASE="https://s3-eu-west-1.amazonaws.com/ledger-lib-ledger-core"
-LIBCORE_VERSION="3.5.0-rc-3d380f"
+LIBCORE_VERSION="4.0.0-rc-44b1f7"
 LIBCORE_DL_DIR=$CURRENT_DIR/../lib-ledger-core-dl
 LIBCORE_SRC_DIR=$CURRENT_DIR/../lib-ledger-core
 
